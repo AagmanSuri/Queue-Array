e01b0d0bff5be5ddcb4d72470cf6e1379f816c50
AagmanSuri <63902687+AagmanSuri@users.noreply.github.com>
2020-12-01 16:01:16 +0530

    create

--- Queue Array/main.c
+++ Queue Array/main.c
@@ -1,14 +1,26 @@
-//
-//  main.c
-//  Queue Array
-//
-//  Created by Aagman Suri on 01/12/20.
-//
-
 #include <stdio.h>
+#include <stdlib.h>
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    printf("Hello, World!\n");
+struct Queue
+{
+    int *size;
+    int *front;
+    int *Rear;
+    int *Q;
+};
+
+void Create(struct Queue *q,int size)
+{
+    q->size=size;
+    q->front=q->Rear=-1;
+    q->Q=(int *) malloc(sizeof(int));
+    
+}
+
+
+int main()
+{
+    struct Queue q;
+    Create(q, 5);
     return 0;
 }
