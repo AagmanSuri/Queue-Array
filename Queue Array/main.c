2ea4d70a9eeb725421e1c47267a4c427908fac41
AagmanSuri <63902687+AagmanSuri@users.noreply.github.com>
2020-12-01 16:39:41 +0530

    Update main.c

--- Queue Array/main.c
+++ Queue Array/main.c
@@ -3,11 +3,11 @@
 
 struct Queue
 {
-    int *size;
-    int *front;
-    int *Rear;
+    int size;
+    int front;
+    int Rear;
     int *Q;
-};
+}*q;
 
 void Create(struct Queue *q,int size)
 {
@@ -16,11 +16,53 @@ void Create(struct Queue *q,int size)
     q->Q=(int *) malloc(sizeof(int));
     
 }
-
-
+void enqueue(struct Queue *q,int x)
+{
+    if(q->Rear==q->size-1)
+    {
+    printf("Queue is full");
+    }
+    else
+    {
+        q->Rear++;
+        q->Q[q->Rear]=x;
+    }
+}
+int Dequeue()
+{
+    int x=-1;
+    if(q->front==q->Rear)
+    {
+        printf("queue is Empty");
+    }
+    else
+    {
+        q->front++;
+        x=q->Q[q->front];
+    }
+    return x;
+}
+void Display(struct Queue q)
+{
+    int i;
+    for(i=q.front;i<=q.Rear;i++)
+    {
+        printf("%d ",q.Q[i]);
+        
+    }
+    printf("\n");
+}
 int main()
 {
     struct Queue q;
-    Create(q, 5);
+    
+    Create(&q, 5);
+    
+    enqueue(&q, 10);
+    enqueue(&q, 20);
+    enqueue(&q, 30);
+    
+    Display(q);
     return 0;
 }
+
